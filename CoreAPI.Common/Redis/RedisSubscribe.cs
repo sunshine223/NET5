4cef910f1f99636332aff1314235a2d8e509c212
波 文 <1083471683@qq.com>
2020-12-07 16:06:27 +0800

    版本更新1

--- CoreAPI.Common/Redis/RedisSubscribe.cs
+++ CoreAPI.Common/Redis/RedisSubscribe.cs
@@ -12,7 +12,6 @@ namespace CoreAPI.Common.Redis
         private async Task SubRedisLoging(string msg)
         {
             Console.WriteLine($"订阅者 1 从 队列{RedisMqKey.Loging} 消费到/接受到 消息:{msg}");
-
             await Task.CompletedTask;
         }
     }
